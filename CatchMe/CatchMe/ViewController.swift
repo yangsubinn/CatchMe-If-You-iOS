384672580e5563d2a1d184fb4c481ffe03d480a6
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-06-28 23:40:21 +0900

    CatchMe Init Setting

---
+++ CatchMe/CatchMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchMe
+//
+//  Created by SHIN YOON AH on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

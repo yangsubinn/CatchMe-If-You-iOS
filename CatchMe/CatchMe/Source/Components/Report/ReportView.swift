13ef13274461cb249d283b655a769a20d5815327
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-05 22:41:15 +0900

    [Add] Report뷰 상단 부분 레이아웃 완료(#6)

--- CatchMe/CatchMe/Source/Components/Report/ReportView.swift
+++ CatchMe/CatchMe/Source/Components/Report/ReportView.swift
@@ -7,15 +7,40 @@
 
 import UIKit
 
-class ReportView: UIView {
+import SnapKit
 
+class ReportView: UIView {
+    // MARK: - Properties
+    let titleView = TitleView(nickname: "이지님")
+    /// character는 후에 KingFisher로 해야 할 듯
+    let characterView = CharacterView(catching: 7, name: "자유로이 떠도는 탐험가", character: "")
+
+    // MARK: - Life Cycle
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupLayout()
         configUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Custom Methods
+    private func setupLayout() {
+        addSubviews([titleView, characterView])
+        
+        characterView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(31)
+            make.leading.trailing.equalToSuperview().inset(28)
+            make.height.equalTo(136)
+        }
+        
+        titleView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(28)
+            make.bottom.equalToSuperview().inset(188)
+            make.height.equalTo(52)
+        }
     }
     
     private func configUI() {

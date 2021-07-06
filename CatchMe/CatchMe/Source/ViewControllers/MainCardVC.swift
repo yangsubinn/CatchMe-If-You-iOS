82a5ec58ac93f446861bfacd742cf204e2feace7
yangsubinn <cindy010670@gmail.com>
2021-07-06 15:10:19 +0900

    [Add] MainCardVC 파일 추가(#24)

---
+++ CatchMe/CatchMe/Source/ViewControllers/MainCardVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainCardVC.swift
+//  CatchMe
+//
+//  Created by 양수빈 on 2021/07/06.
+//
+
+import UIKit
+
+class MainCardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

b22803853a454acb9684ab2cb483d0aa7f1731d1
yangsubinn <cindy010670@gmail.com>
2021-07-08 03:10:32 +0900

    [Feat] SettingView tableView layout 구현(#37)

---
+++ CatchMe/CatchMe/Source/Cells/Setting/SettingTVC.swift
@@ -0,0 +1,34 @@
+//
+//  SettingTVC.swift
+//  CatchMe
+//
+//  Created by 양수빈 on 2021/07/07.
+//
+
+import UIKit
+
+class SettingTVC: UITableViewCell {
+    static let identifier = "SettingTVC"
+    
+    // MARK: - Properties
+    let titleLabel = UILabel()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupLayout()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: - Custom Method
+    func setupLayout() {
+        addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(22)
+        }
+    }
+}
--- CatchMe/CatchMe/Source/ViewControllers/SettingVC.swift
+++ CatchMe/CatchMe/Source/ViewControllers/SettingVC.swift
@@ -13,17 +13,21 @@ class SettingVC: UIViewController {
     //MARK: - Properties
     lazy var backButton = BackButton(self)
     let titleLabel = UILabel()
+    let settingTableView = UITableView(frame: .zero, style: .insetGrouped)
+    
+    private let sectionList: [String] = ["닉네임 변경", "비밀번호 변경", "이용약관", "오픈소스 라이선스", "로그아웃", "서비스 탈퇴"]
 
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
         configUI()
+        setTableView()
     }
-    
+
     //MARK: - Custom Method
     func setupLayout() {
-        view.addSubviews([backButton, titleLabel])
+        view.addSubviews([backButton, titleLabel, settingTableView])
         
         backButton.snp.makeConstraints { make in
             make.top.equalTo(view.snp.top).offset(55)
@@ -35,6 +39,14 @@ class SettingVC: UIViewController {
             make.top.equalTo(view.snp.top).offset(66)
             make.centerX.equalTo(view.snp.centerX)
             make.centerY.equalTo(backButton.snp.centerY)
+        }
+        
+        settingTableView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
+            // tableView에 기본으로 잡혀있는 좌우패딩이 있나 왜이래
+            make.leading.equalToSuperview().offset(28)
+            make.trailing.equalToSuperview().inset(28)
+            make.bottom.equalToSuperview()
         }
     }
     
@@ -48,8 +60,49 @@ class SettingVC: UIViewController {
         titleLabel.font = UIFont.stringBoldSystemFont(ofSize: 20)
     }
     
-    func setupTableView() {
+    func setTableView() {
+        settingTableView.backgroundColor = .blue
         
+        settingTableView.dataSource = self
+        settingTableView.delegate = self
+        
+        settingTableView.register(SettingTVC.self, forCellReuseIdentifier: "SettingTVC")
+        
+        settingTableView.separatorStyle = .none
+        settingTableView.tableFooterView = UIView(frame: .zero)
+        settingTableView.sectionFooterHeight = 0
     }
+}
 
+// MARK: - UITableViewDelegate
+extension SettingVC: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionList.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Value: \(sectionList[indexPath.section])")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 54
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SettingVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTVC", for: indexPath)
+        
+        cell.textLabel?.text = "\(sectionList[indexPath.section])"
+        cell.backgroundColor = .black200
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = .stringRegularSystemFont(ofSize: 16)
+
+        return cell
+    }
 }

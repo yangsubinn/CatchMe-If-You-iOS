826cb159071858b3dd18be02f73da82e65ce28d5
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-10 02:55:02 +0900

    [Chore] mark 변경

--- CatchMe/CatchMe/Source/Extensions/UIApplication+Extension.swift
+++ CatchMe/CatchMe/Source/Extensions/UIApplication+Extension.swift
@@ -9,7 +9,6 @@ import UIKit
 
 extension UIApplication {
     static var statusBarHeight: CGFloat {
-        /// #available이 안먹어요..
 //        if #available(iOS 13.0, *) {
 //            let window = shared.windows.filter { $0.isKeyWindow }.first
 //            return window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0

094e14c1d117c1e665f5bf8a4702b6485ec38ec3
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-09 15:59:58 +0900

    [Chore] TextField 코드 변경(#44)

--- CatchMe/CatchMe/Source/Components/Shared/CustomTextField.swift
+++ CatchMe/CatchMe/Source/Components/Shared/CustomTextField.swift
@@ -11,10 +11,10 @@ import SnapKit
 
 class CustomTextField: UITextField {
 
-    init(placeholder: String) {
+    init(placeholder: String, isAddCatchu: Bool? = false) {
         super.init(frame: .zero)
-        setupLayout()
-        configUI(placeholder: placeholder)
+        setupLayout(isAddCatchu ?? false)
+        configUI(placeholder: placeholder, isAddCatchu ?? false)
         removeAuto()
     }
     
@@ -22,21 +22,21 @@ class CustomTextField: UITextField {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupLayout() {
+    private func setupLayout(_ isAddCatchu: Bool) {
         self.snp.makeConstraints { make in
-            make.height.equalTo(48)
+            make.height.equalTo(isAddCatchu ? 48 : 56)
         }
     }
     
-    private func configUI(placeholder: String) {
+    private func configUI(placeholder: String, _ isAddCatchu: Bool = false) {
         backgroundColor = .black200
         textColor = .white
         
-        layer.cornerRadius = 13
+        layer.cornerRadius = isAddCatchu ? 13 : 20
         layer.borderWidth = 1
         layer.borderColor = UIColor.clear.cgColor
         
-        attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray400])
+        attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : isAddCatchu ? UIColor.gray400 : UIColor.gray100])
         font = .systemFont(ofSize: 16, weight: .medium)
         
         setLeftPaddingPoints(17)
@@ -56,5 +56,9 @@ class CustomTextField: UITextField {
     
     func setupPinkLine() {
         layer.borderColor = UIColor.pink100.cgColor
+    }
+    
+    func changePaddingPoints(point: CGFloat) {
+        setLeftPaddingPoints(point)
     }
 }
--- CatchMe/CatchMe/Source/ViewControllers/LoginVC.swift
+++ CatchMe/CatchMe/Source/ViewControllers/LoginVC.swift
@@ -12,6 +12,8 @@ import SnapKit
 class LoginVC: UIViewController {
     // MARK: - Properties
     let logoImageView = UIImageView()
+    let emailTextField = CustomTextField(placeholder: "이메일")
+    let passwordTextField = CustomTextField(placeholder: "비밀번호")
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -22,7 +24,7 @@ class LoginVC: UIViewController {
     
     // MARK: - Custom Method
     private func setupLayout() {
-        view.addSubviews([logoImageView, ])
+        view.addSubviews([logoImageView, emailTextField, passwordTextField])
         
         logoImageView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).inset(91)
@@ -30,9 +32,23 @@ class LoginVC: UIViewController {
             make.height.equalTo(100)
             make.width.equalTo(282)
         }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(69)
+            make.leading.trailing.equalToSuperview().inset(28)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(36)
+            make.leading.trailing.equalTo(emailTextField)
+        }
     }
     
     private func configUI() {
+        view.backgroundColor = .black100
         logoImageView.backgroundColor = .systemTeal
+        
+        emailTextField.changePaddingPoints(point: 50)
+        passwordTextField.changePaddingPoints(point: 50)
     }
 }

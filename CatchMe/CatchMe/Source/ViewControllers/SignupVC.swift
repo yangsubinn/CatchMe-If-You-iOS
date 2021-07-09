9a06baaff05eac8383939cf5d7973dc5c736f985
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-10 00:29:41 +0900

    [Add] signupVC 추가(#46)

---
+++ CatchMe/CatchMe/Source/ViewControllers/SignupVC.swift
@@ -0,0 +1,29 @@
+//
+//  SignupVC.swift
+//  CatchMe
+//
+//  Created by SHIN YOON AH on 2021/07/10.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

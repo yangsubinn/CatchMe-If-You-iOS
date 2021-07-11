d6cb0bd4f2ad09773666b56fdd23c9886dda0676
yangsubinn <cindy010670@gmail.com>
2021-07-12 03:37:37 +0900

    [Feat] LookView 상단 레이아웃 구현(#77)

--- CatchMe/CatchMe/Source/ViewControllers/LookVC.swift
+++ CatchMe/CatchMe/Source/ViewControllers/LookVC.swift
@@ -20,7 +20,8 @@ class LookVC: UIViewController {
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupLayout()
+        configUI()
     }
     
     //MARK: - Custom Method
@@ -33,12 +34,39 @@ class LookVC: UIViewController {
         }
         
         backButton.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top).offset(55)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(UIScreen.main.hasNotch ? 11 : 20)
             make.leading.equalToSuperview().offset(14)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(backButton.snp.bottom).offset(25)
+            make.leading.equalToSuperview().offset(UIScreen.main.hasNotch ? 28 : 24)
+        }
+        
+        subLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(UIScreen.main.hasNotch ? 28 : 24)
+        }
+        
+        topImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(UIScreen.main.hasNotch ? 84 : 90)
+            make.trailing.equalToSuperview().inset(18)
+            make.width.equalTo(82)
+            make.height.equalTo(64)
         }
     }
     
     func configUI() {
+        view.backgroundColor = .black100
+        topBackView.backgroundColor = .brown
+        topImageView.backgroundColor = .yellow
         
+        titleLabel.text = "다른 유저들의 캐츄"
+        titleLabel.textColor = .white
+        titleLabel.font = .stringBoldSystemFont(ofSize: 22)
+        
+        subLabel.text = "블라너ㅓ미허ㅣ머ㅏ미"
+        subLabel.textColor = .white
+        subLabel.font = .stringMediumSystemFont(ofSize: 14)
     }
 }

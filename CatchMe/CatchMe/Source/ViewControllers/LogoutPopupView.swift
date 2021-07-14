20ed7b86ddbd785839babc4b0557109d330d40d6
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-14 18:03:55 +0900

    [Add] Popup 생성(#66)

---
+++ CatchMe/CatchMe/Source/ViewControllers/LogoutPopupView.swift
@@ -0,0 +1,20 @@
+//
+//  LogoutPopupView.swift
+//  CatchMe
+//
+//  Created by SHIN YOON AH on 2021/07/14.
+//
+
+import UIKit
+
+class LogoutPopupView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

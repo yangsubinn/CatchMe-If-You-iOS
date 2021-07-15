e6106c8b2c3b8e61a56ebc87906e8ce0c1b65cab
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-15 20:30:08 +0900

    [Feat] Edit 통신 구현 완료(#131)

---
+++ CatchMe/CatchMe/Source/Models/Character/EditCharacterModel.swift
@@ -0,0 +1,15 @@
+//
+//  EditCharacterModel.swift
+//  CatchMe
+//
+//  Created by SHIN YOON AH on 2021/07/15.
+//
+
+import Foundation
+
+// MARK: - EditCharacterModel
+struct EditCharacterModel: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+}

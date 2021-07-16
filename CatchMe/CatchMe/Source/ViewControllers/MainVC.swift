d765a87c4298aa8de8fcf5ccdc1b2ecc1d4266b3
yangsubinn <cindy010670@gmail.com>
2021-07-16 22:53:58 +0900

    [Fix] 메인뷰 카드상세 연결 인덱스 수정

--- CatchMe/CatchMe/Source/ViewControllers/MainVC.swift
+++ CatchMe/CatchMe/Source/ViewControllers/MainVC.swift
@@ -418,6 +418,7 @@ extension MainVC {
                     activitys.removeAll()
                     totals.removeAll()
                     characters.removeAll()
+                    indexs.removeAll()
                     
                     data.append(contentsOf: characterData?.data ?? [])
                      
@@ -473,6 +474,12 @@ extension MainVC {
 
                         collectionView.reloadData()
                         pageControl.pages = names.count
+                        
+                        print("----------------------------------------------")
+                        print(names)
+//                        print(levels)
+                        print(indexs)
+                        print("----------------------------------------------")
                         
                         if !names.isEmpty {
                             nameLabel.text = names[0]

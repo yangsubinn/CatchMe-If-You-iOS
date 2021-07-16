3314f5c47b9fdf223a343beb5359ca27d8fd036a
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-16 16:55:38 +0900

    [Chore] accessToken 맞춤(#153)

--- CatchMe/CatchMe/Resource/APIServices/Services/LookService.swift
+++ CatchMe/CatchMe/Resource/APIServices/Services/LookService.swift
@@ -48,10 +48,11 @@ extension LookService: TargetType {
     }
 
     var headers: [String: String]? {
+        let accessToken = UserDefaultStorage.accessToken
         switch self {
         default:
             return ["Content-Type": "application/json",
-                    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjBlNWQzMTE0ZjM3ZTliMjUyYzYwOGJlIn0sImlhdCI6MTYyNjI2MDUxMSwiZXhwIjoxNjI3NDcwMTExfQ.mAni2lnF47sgNnQinxi-DTT-Vknf6KP7CmhCBf5VmLI"] // GeneralAPI.token
+                    "token": accessToken] // GeneralAPI.token
         }
     }
 }
